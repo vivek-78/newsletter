f7e6c8188bab2c223dfad249c933c75335fdc4f3
VIVEK <vivek@VIVEKs-MacBook-Air.local>
2021-04-29 01:15:42 +0530

    3

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
-const express    = require("express");
+const express = require("express");
+const http =  require("https");
 const bodyParser = require("body-parser");
-const request    = require("request");
 
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
-app.get("/" , function(req , res)
-{
+app.get("/" , function(req , res){
+
     res.sendFile(__dirname + "/index.html");
-});
+})
 
-app.post("/" , function(req , res){
-    var firstname = req.body.firstName;
-    var lastname = req.body.lastName;
-    var email = req.body.email;
-    console.log(firstname , lastname , email);
+app.post("/" , function( req , res){
 
-    const url = "https://us1.api.mailchimp.com/export/1.0."
+    const fn = req.body.firstName;
+    const ln = req.body.lastName;
+    const email = req.body.email;
 
+    console.log(fn , ln , email)
+
+    const data = {
+        members : [
+            {
+            email_address : email,
+            status : "subscribed",
+            merge_fields : {
+            FNAME : fn,
+            LNAME : ln,
+            }
+        }
+        ]
+    }
     const jsonData = JSON.stringify(data);
+    
+    const url = "https://us1.api.mailchimp.com/3.0/lists/c38e3021fe"
 
+    const options = {
 
+        method : "POST",
+        auth : "vivek:4264e2bb22c5534b69b598a995fb5675-us1"
+    }
 
-});
+    const request = http.request(url , options , function(response){
+        response.on("data" , function(data){
+            console.log(JSON.parse(data))
+        })
+    })
 
-app.listen(process.env.PORT, function(){
- console.log("your server is running on port 1000");
-});
+    request.write(jsonData)
+    request.end();
+})
+
+app.listen(8080 , function(){
+    console.log("your server is running on port 8080");
+})
 
 // api key : 91e5716b0eb8088f7322a508fd6c8f84-us1
-// audience key : c38e3021fe
+// list id : list id : c38e3021fe 
